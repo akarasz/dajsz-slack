8159b7432a4b5fe4e9810c4caa046148b663d83d
Andras Karasz <andras.karasz@logmein.com>
2021-01-14 08:56:47 +0100

    feat: actual game creation and giving back a link to that

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -21,18 +21,20 @@ func main() {
 
 	flag.Parse()
 
+	api := slack.New(oauthToken)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		verifier, err := slack.NewSecretsVerifier(r.Header, signingSecret)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			logError(w, "unable to create secrets verifier", err)
 			return
 		}
 
 		r.Body = ioutil.NopCloser(io.TeeReader(r.Body, &verifier))
 		s, err := slack.SlashCommandParse(r)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			logError(w, "unable to parse command", err)
 			return
 		}
 
@@ -43,25 +45,48 @@ func main() {
 
 		switch s.Command {
 		case "/dajsz":
-			api := slack.New(oauthToken)
+			res, err := http.Post("https://api.dajsz.hu/", "", nil)
 
-			channelID, timestamp, err := api.PostMessage(
+			if err != nil {
+				logError(w, "failed to post request", err)
+				return
+			}
+			if res.StatusCode != http.StatusCreated {
+				logError(w, "wrong return from api", res.StatusCode)
+				return
+			}
+
+			gameIDs, ok := res.Header["Location"]
+			if !ok {
+				logError(w, "no location header in api response", nil)
+				return
+			}
+			if len(gameIDs) != 1 {
+				logError(w, "invalid location header in api response", gameIDs)
+				return
+			}
+
+			_, _, err = api.PostMessage(
 				s.ChannelID,
-				slack.MsgOptionText("You shoud _definitely_ play a game :game_die:", false),
+				slack.MsgOptionText("Join a friendly game of yahtzee: https://dajsz.hu/#" + gameIDs[0], false),
 				slack.MsgOptionAsUser(true),
 			)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			log.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 			w.WriteHeader(http.StatusOK)
 			return
 		default:
-			w.WriteHeader(http.StatusInternalServerError)
+			logError(w, "invalid command", s.Command)
 			return
 		}
 	})
 
 	http.ListenAndServe(":3000", nil)
+}
+
+func logError(w http.ResponseWriter, msg string, details interface{}) {
+	log.Printf("%s: %v", msg, details)
+	w.WriteHeader(http.StatusInternalServerError)
 }
