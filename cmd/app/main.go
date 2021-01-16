bd44dd8b5196c82b57eb89778c3e67feba032979
Andras Karasz <andras.karasz@logmein.com>
2021-01-16 19:04:58 +0100

    fix: explicit redirect to success page at end of auth

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -89,12 +89,14 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 		os.Getenv("CLIENT_ID"),
 		os.Getenv("CLIENT_SECRET"),
 		code[0],
-		"https://slack.dajsz.hu/auth/success")
+		"")
 	if err != nil {
 		logError(w, "failed to get oauth token", err)
 		return
 	}
 
+	w.Header().Set("Location", "https://slack.dajsz.hu/auth/success")
+	w.WriteHeader(http.StatusFound)
 	w.WriteHeader(http.StatusOK)
 }
 
